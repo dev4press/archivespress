5531748e2f447996f2fd2e67b43e935bb5426e27
Milan Petrovic <milan@gdragon.info>
2026-09-03 16:21:42 +0200

    [scripts] [security] Updated package. Updated requirements.

--- core/basic/Information.php
+++ core/basic/Information.php
@@ -7,20 +7,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Information {
-	public $code = 'archivespress';
+	public string $code = 'archivespress';
 
-	public $version = '3.4';
-	public $build = 34;
-	public $edition = 'free';
-	public $status = 'stable';
-	public $updated = '2026.07.19';
-	public $released = '2021.04.22';
+	public string $version = '3.5';
+	public int $build = 35;
+	public string $edition = 'free';
+	public string $status = 'stable';
+	public string $updated = '2026.09.03';
+	public string $released = '2021.04.22';
 
-	public $author_name = 'Milan Petrovic';
-	public $author_url = 'https://www.dev4press.com/';
+	public string $author_name = 'Milan Petrovic';
+	public string $author_url = 'https://www.dev4press.com/';
 
-	public $php = '8.0';
-	public $wordpress = '6.4';
+	public string $php = '8.0';
+	public string $wordpress = '6.4';
 
 	public static function instance() : Information {
 		static $instance = null;
